647203e83f7200a52ac8c0f2cc63e268f2a0da71
hjr <2327553735@qq.com>
2021-12-07 16:51:44 +0800

    fix：围棋存盘

--- 围棋存盘/src/SpareArray.java
+++ 围棋存盘/src/SpareArray.java
@@ -21,5 +21,28 @@ public class SpareArray {
                 }
             }
         }
+        System.out.println(sum);
+        int[][] spareArray = new int[sum + 1][3];
+        spareArray[0][0] = 11;
+        spareArray[0][1] = 11;
+        spareArray[0][2] = sum + 1;
+        int count = 0;//用于记录第几个非0数据
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (ints[i][j] != 0) {
+                    count++;
+                    spareArray[count][0] = i;
+                    spareArray[count][1] = j;
+                    spareArray[count][2] = ints[i][j];
+                }
+            }
+        }
+
+        for (int[] row : spareArray) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
     }
 }
